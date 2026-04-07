f1b9b39f2f4ac6e10f3ca4b419e5e97aae7eb00a
raniachahbouni769-cloud <raniachahbouni769@gmail.com>
2026-04-08 01:48:14 +0200

    new2

--- home.js
+++ home.js
@@ -21,3 +21,16 @@ goBtn.addEventListener('click', () => {
         alert("Searching for vibes related to: " + searchInput.value);
     }
 });
+ function selectOption(element) {
+    // 1. Remove 'selected' class from all cards
+    document.querySelectorAll('.option-card').forEach(card => {
+        card.classList.remove('selected');
+    });
+
+    // 2. Add 'selected' class to the one we clicked
+    element.classList.add('selected');
+
+    // 3. Optional: Print the choice to console
+    const choice = element.querySelector('p').innerText;
+    console.log("User selected mood: " + choice);
+}
