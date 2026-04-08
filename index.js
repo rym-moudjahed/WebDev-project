a456f3f68671be76e13bbb042853a8803389d171
Nesrinevik <ltnesrine05@gmail.com>
2026-04-08 23:15:24 +0100

    update 1

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 30,
+      slidesPerView: 4,
+      spaceBetween: 20,
+      loop: true ,
       pagination: {
         el: ".swiper-pagination",
         clickable: true,
       },
     });
+const menubutton = document.getElementById('menubutton');
+const btn = document.getElementById('btn');
+const sidebar = document.getElementById('sidebar');
+menubutton.onclick = function() {
+    sidebar.classList.add('active');
+        
+};
+btn.onclick = function() {
+    sidebar.classList.remove('active');
+    
+};
